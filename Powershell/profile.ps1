13cdbca08c270995f83956b3821f73d2b9efce3f
Adrian Banks <github@adrianbanks.co.uk>
2026-03-09 10:57:52 +0000

    Add Posh Git

--- Powershell/profile.ps1
+++ Powershell/profile.ps1
@@ -1,3 +1,5 @@
+Import-Module posh-git
+
 oh-my-posh init pwsh --config ~/.ohmyposh/ANB.omp.json | Invoke-Expression
 
 Import-Module PSReadLine
@@ -6,3 +8,4 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 Function Remove-Build { Remove-Item -LiteralPath Build -Force -Recurse }
 Function Remove-BinObj { Get-ChildItem -Path .\ -Include bin,obj -Directory -Recurse | ? { $_.FullName -inotmatch '\\packages\\' } | foreach ($_) { Write-Output $_.fullname; Remove-Item $_.fullname -Force -Recurse } }
 Function Remove-Bin { Get-ChildItem -Path .\ -Include bin -Directory -Recurse | ? { $_.FullName -inotmatch '\\packages\\' } | foreach ($_) { Write-Output $_.fullname; Remove-Item $_.fullname -Force -Recurse } }
+
